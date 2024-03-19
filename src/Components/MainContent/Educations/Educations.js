f7b14b593f2f2d407a4abdfd9c03021649cab898
Ahmed Khemici <ahmed.khemici@yib.io>
2024-03-19 02:19:35 +0100

    99% done

--- src/Components/MainContent/Educations/Educations.js
+++ src/Components/MainContent/Educations/Educations.js
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faUpRightFromSquare} from "@fortawesome/free-solid-svg-icons"
 
 const Educations = (props) => {
-  const educations = [1,2].map(education => {
-    return (<div className="educations__degree">
-      <div className="">
+  const educations = [1].map(education => {
+    return (<div className="educations__education">
+      <div className="educations__educationDegree">
+        Computer Networking and Information Technology master's degree
         {education.degree}
       </div>
-      <div className="">
-        <div className="educations__collegeName">
+      <div className="educations__educationCollege">
+        <div className="educations__educationCollegeName">
           Saad Dahleb Blida
           {education.college_name}
         </div>
@@ -17,29 +18,31 @@ const Educations = (props) => {
           <FontAwesomeIcon icon={faUpRightFromSquare} />
         </a>
       </div>
-      <div className="">
-        <div className="">
+      <div className="educations__educationDateGrade">
+        <div className="educations__educationDate">
           09/2018 – 09/2019
           {education.date}
         </div>
-        <div className="">
+        <div className="educations__educationGrade">
           GPA 3.5
           {education.grade}
         </div>
       </div>
-      <div className="">
-        <div>
-          Project
-        </div>
-        <ul>
-        {
-          [1,2].map(project => {
-            return <li>
-
-            </li>
-          })
-        }
-        </ul>
+      <div className="educations__educationProjects">
+          <span>
+            Project
+          </span>
+          <ul>
+          {
+            [1].map(project => {
+              return <li>
+                Contour Parallel Offset and laser pathing
+                using the Algorithm for Pocketing in order to
+                fill 2D Motifs
+              </li>
+            })
+          }
+          </ul>
       </div>
     </div>
     )
