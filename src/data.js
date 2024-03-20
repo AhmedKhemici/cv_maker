67efda5f9bbcd60a8c65ab91978ee90e641c11c7
Ahmed Khemici <ahmed.khemici@yib.io>
2024-03-20 04:44:17 +0100

    Good

--- src/data.js
+++ src/data.js
@@ -1,31 +1,28 @@
 export const data = {
     "language":"eng",
     "profile": {
-        "first_name": "Abdou",
-        "last_name": "Chabira",
+        "profile_photo": "",
+        "first_name": "Ahmed",
+        "last_name": "Khemici",
         "profession": "Software Engineer",
-        "description":`Experienced software engineer proficient in Python, Flask, and relational databases (PostgreSQL, MySQL). 
-        Skilled in developing robust server-side components, building RESTful APIs, 
-        and integrating third-party services (HubSpot, Zapier, Pipedrive). 
-        Seeking opportunities to leverage strong problem-solving skills in a Data Engineering role.
-        `
+        "description":``
     },
     "pages":[{
         "side_content": {
             "medias": [
                 {
                     "type": "email",
-                    "value": "abdou-chabira@outlook.com",
-                    "url": "mailto:abdou-chabira@outlook.com"
+                    "value": "ahmed.khemici1@gmail.com",
+                    "url": "mailto:ahmed.khemici1@gmail.com"
                 },
                 {
                     "type": "phone",
-                    "value": "+213778376433"
+                    "value": "+213665824855"
                 },
                 {
                     "type": "whatsapp",
-                    "value": "+213778376433",
-                    "url": "https://wa.me/+213778376433"
+                    "value": "+213665824855",
+                    "url": "https://wa.me/+213665824855"
                 },
                 {
                     "type": "location",
@@ -34,26 +31,32 @@ export const data = {
                 },
                 {
                     "type": "linkedin",
-                    "value": "inkedin.com/in/abderrahmane-chabira-924507123",
-                    "url": "https://www.linkedin.com/in/abderrahmane-chabira-924507123"
+                    "value": "linkedin.com/in/ahmed-khemici-4523aa156",
+                    "url": "https://linkedin.com/in/ahmed-khemici-4523aa156"
                 }
             ],
             "skills": [
                 {
-                    "name": "Frameworks:",
-                    "tools": "Python, Flask"
+                    "name": "Programming Languages :",
+                    "tools": "Python ,javascript, java"
+                },
+                {
+                    "name": "Frameworks :",
+                    "tools": "Flask, NodeJs, ReactJs, React Native"
                 },
                 {
                     "name": "DataBases :",
-                    "tools": "MySQL , PostgreSQL, Relational Databases"
+                    "tools": "PostgreSQL, MySQL, SQL server, MongoDB, Redis"
                 },
                 {
-                    "name": "Microsoft Office :",
-                    "tools": " Amazon Web Services (AWS), Azure"
+                    "name": "DevOps Tools:",
+                    "tools": `AWS services (EC2, Target Groups, CloudWatch, cloudfront
+                    , S3, LoadBalancers, RDS, ECR, ASG), AZURE(VMs, ApplicationGateways, DB, RedisDB),
+                    Docker, Ansible, GitLab_CI_CD, GitHunActions.`
                 },
                 {
                     "name": "Others :",
-                    "tools": "Git,  REST APIs, Customer Relationship Management (CRM)"
+                    "tools": "Scrum, Atlassian, Team Player"
                 }
             ],
             "languages": [
@@ -74,104 +77,39 @@ export const data = {
         "main_content": {
             "experiences": [
                 {
-                    "job_title": "Back End Developer",
-                    "company_name": "Fintechracy",
-                    "company_url": "https://www.fintechracy.org/",
-                    "date": "10/2023 - 02/2024",
+                    "job_title": "",
+                    "company_name": "",
+                    "company_url": "",
+                    "date": "",
                     "location": "FreeLance",
                     "points": [
-                        "Design and implement RESTful APIs for seamless communication between the frontend and backend.",
-                        "Ensure API security and integration with third-party services.",
-                        "Integrated social login functionalities (Apple, Google, Facebook) to enhance user accessibility and improve the overall user experience.",
-                        "Proficient in Python, the primary language used in Django development."
-                    ]
-                },
-                {
-                    "job_title": "Information Technology Engineer",
-                    "company_name": "Caisse Nationale des Retraites (C.N.R)",
-                    "date": "08/2023 - Present",
-                    "location": "Algeria",
-                    "points": [
-                        "System Administration.",
-                        "Database Management.",
-                        "Hardware and Software Troubleshooting.",
-                        "Automation and Scripting.",
-                        "Documentation and Reporting",
-                    ]
-                },
-                {
-                    "job_title": "Software Engineer",
-                    "company_url": "https://yobi.app/",
-                    "company_name": "Yobi",
-                    "date": "12/2020 - 08/2023",
-                    "location": "YiB Inc • Silicon Valley, United States",
-                    "points": [
-                        "Design, develop, and maintain high-performance, scalable, and reliable server-side components for Yobi's software applications and systems.",
-                        "Implemented RESTful APIs and web services, ensuring seamless communication between Yobi's web app and various integrations (HubSpot, Zapier, Pipedrive, Facebook messaging and posts, Instagram DMs, LinkedIn, Zoom).",
-                        "Integrated social login functionalities (Apple, Google, Facebook) to enhance user accessibility and improve the overall user experience.",
-                        "Utilize Agile development processes, participating in sprint planning, stand-ups, and retrospectives to ensure efficient project execution."
-                    ]
-                },
-                {
-                    "job_title": "Software Engineer",
-                    "company_name": "Call Sumo",
-                    "date": "02/2021 - 11/2021",
-                    "location": "Call Sumo • San Jose, United States",
-                    "points": [
-                        "Developed an intuitive admin dashboard, streamlining management operations and providing efficient monitoring and control for Call Sumo",
-                        "Implemented automated call transcription and sentiment analysis using Python, Flask, and MySQL, enabling data-driven insights.",
-                        "Visualize data with Google Data Studio, creating Apps script apps for clear and actionable reports.",
-                        "Collaborate with cross-functional teams to deliver a seamless end-to-end user experience."
-                    ]
-                }
-            ],
-        }
-    },
-    {
-        "main_content": {
-            "experiences": [
-                {
-                    "job_title": "Software Engineer",
-                    "company_name": "Sarl Minoteries El-Baraka",
-                    "date": "03/2020 - 12/2020",
-                    "location": "Algeria",
-                    "points": [
-                        "Design and develop a web application for HR management, optimizing employee data management and administrative processes.",
-                        "Install robust computer networks to ensure reliable connectivity and data access for the organization",
-                        "Provide technical guidance and training on computer and network usage, fostering a tech-savvy work environment.",
-                        "Demonstrate strong system administration skills, ensuring the smooth functioning of critical IT infrastructure."
+                       
                     ]
                 }
             ],
             "certificates": [
                 {
-                    "name": "master degree software engineering - university mohamed khider",
-                    "date": "2016 - 2018"
-                },
-                {
-                    "name": "Cybersecurity - Cisco",
-                    "date": "1002807735"
+                    "name": "",
+                    "date": ""
                 }
+            ],
+            "personal_projects": [
+                {
+                    "name": "",
+                    "points": [
+
+                    ]
+                },
             ],
             "educations": [
                 {
-                    "degree": "Master degree, Software engineering and distributed systems",
-                    "college_name": "University of Mohamed Khieder",
-                    "college_url": "https://maps.app.goo.gl/vCSiiFrU51pLVtga6",
-                    "date": "2016 - 2018",
-                    "grade": "GPA 3.7",
+                    "degree": "",
+                    "college_name": "",
+                    "college_url": "",
+                    "date": "",
+                    "grade": "",
                     "projects": [
-                        "developed a tool for modeling and verifying reconfigurable petri nets."
-                    ]
-                },
-                {
-                    "degree": "Bachelor degree, Computer science",
-                    "college_name": "University of Mohamed Khieder",
-                    "college_url": "https://maps.app.goo.gl/vCSiiFrU51pLVtga6",
-                    "date": "2013 - 2016",
-                    "grade": "GPA 3.5",
-                    "projects": [
-                        "developed a facial recognition software (AI)."
+                        ""
                     ]
                 }
             ]
